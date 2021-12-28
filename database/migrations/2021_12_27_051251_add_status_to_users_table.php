abefee04989d9633d4a0611c95a980c68c38817b
Gede Rico Wijaya <gricowijaya@gmail.com>
2021-12-28 17:19:44 +0800

    migration for status in users table

---
+++ database/migrations/2021_12_27_051251_add_status_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('status', ['Married', 'Single', 'In a Relationship'])->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('cover_img')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('status');
+            $table->dropColumn('birthdate');
+            $table->dropColumn('cover_img');
+        });
+    }
+}
