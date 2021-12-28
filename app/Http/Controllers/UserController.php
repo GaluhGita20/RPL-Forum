7a7b2b688e804da656c4016c4bbf9966edcf61ab
Gede Rico Wijaya <gricowijaya@gmail.com>
2021-12-28 17:17:50 +0800

    Create User Controller For Update Topics

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,8 @@ use App\Models\KategoriForum;
 use App\Models\Topic;
 use App\Models\History;
 use Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 
 class UserController extends Controller
@@ -29,12 +31,15 @@ class UserController extends Controller
 
     public function view_profileUser()
     {
-        return view('pages.profileUser');
+        $status = User::getEnumKey('status');
+        $pekerjaan = User::getEnumKey('pekerjaan');
+        $provinsi = Http::get('https://dev.farizdotid.com/api/daerahindonesia/provinsi')->json();
+        return view('pages.profileUser', compact('status', 'pekerjaan', 'provinsi'));
     }
 
     public function view_topTopics()
     {
-        $topics=Topic::join('kategori_forums', 'topics.kategori_forum_id', '=', 'kategori_forums.id')->get(['topics.*', 'kategori_forums.*']);
+        $topics = Topic::join('kategori_forums', 'topics.kategori_forum_id', '=', 'kategori_forums.id')->get(['topics.*', 'kategori_forums.*']);
         return view('pages.top-topics')->with(compact('topics'));
     }
 
@@ -43,5 +48,33 @@ class UserController extends Controller
         $user = Auth::user();
         $histories = History::where('user_id', '=', $user->id);
         return view('pages.history', compact('histories'));
+    }
+
+    public function update_profileUser(Request $request)
+    {
+        $user = Auth::user();
+
+        if ($request->file('profile_avatar')) {
+            $avatar = $request->file('profile_avatar');
+            $fileAvatar = $avatar->storeAs('avatar', 'profile_avatar_' . Str::slug($user->name) . '.' . $avatar->extension());
+            $user->file = $fileAvatar;
+        }
+
+        if ($request->file('profile_cover')) {
+            $cover = $request->file('profile_cover');
+            $fileCover = $cover->storeAs('cover_img', 'cover_img_' . Str::slug($user->name) . '.' . $cover->extension());
+            $user->cover_img = $fileCover;
+        }
+
+        $user->name = $request->profile_name;
+        $user->email = $request->profile_public_email;
+        $user->deskripsi = $request->profile_description;
+        $user->birthdate = $request->profile_birthday;
+        $user->alamat = $request->profile_city;
+        $user->pekerjaan = $request->profile_pekerjaan;
+        $user->status = $request->profile_status;
+        $user->save();
+
+        return back();
     }
 }
